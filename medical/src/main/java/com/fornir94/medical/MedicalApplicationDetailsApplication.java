dbf3025f8b1eb7d44082e512bd1fe70d7dcc1bf8
Fornir94 <138400075+Fornir94@users.noreply.github.com>
2023-07-29 12:17:45 +0200

    Add files via upload

---
+++ medical/src/main/java/com/fornir94/medical/MedicalApplicationDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.fornir94.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalApplicationDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalApplicationDetailsApplication.class, args);
+	}
+
+}
---
+++ medical/src/test/java/com/fornir94/medical/MedicalApplicationDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fornir94.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
